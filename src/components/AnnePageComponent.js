2590a99b0bfeff479d54176bf6112cdcf1e730be
rodrigogarh <garherro@gmail.com>
2019-07-07 22:42:31 -0300

    Add pixel

--- src/components/AnnePageComponent.js
+++ src/components/AnnePageComponent.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import parse from 'html-react-parser';
+import Helmet from 'react-helmet';
 
 import Layout from './layout';
 import SEO from './seo';
@@ -169,6 +170,48 @@ class AnnePageComponent extends React.Component {
 
     return (
       <Layout>
+        <Helmet>
+          <script>
+            {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '374116829696592');
+    fbq('track', 'PageView');`}
+          </script>
+          <noscript>
+            {`<img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=374116829696592&ev=PageView&noscript=1"
+  />`}
+          </noscript>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-814054608" />
+          <script>
+            {`window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-814054608');`}
+          </script>
+          <script>
+            {`function gtag_report_conversion(url) {
+        var callback = function () {
+          if (typeof(url) != 'undefined') {
+            window.location = url;
+          }
+        };
+        gtag('event', 'conversion', {
+            'send_to': 'AW-814054608/Xm6_CI_K5H4Q0PmVhAM',
+            'event_callback': callback
+        });
+        return false;
+      }`}
+          </script>
+        </Helmet>
         <SEO title="Sobre a Anne | Anne Schuartz" keywords={[`doces`, `tortas`, `bolos`]} />
         <HeroSection>
           <div className="as-hero" style={{ padding: '176px 0', margin: '0 auto', maxWidth: 960 }}>
